56569e26ab940c8794d8c7b8821f963bd706a848
AdrianoGamba <adriano-gamba@hotmail.com>
2019-11-15 16:45:24 -0300

    lab3 | Regra de controle implementada. Falta testar e calibrar PI.

--- Pratica/Projects/lab3/src/Controle.c
+++ Pratica/Projects/lab3/src/Controle.c
@@ -8,77 +8,61 @@ void xControleTask(void *arg){
         uint32_t integratState; // Integrator state
         uint32_t integratMax;   // Maximum and minimum
         uint32_t integratMin;   // allowable integrator state
-        int kp;               // integral gain
-        int ki;               // proportional gain
+        int kp;                 // integral gain
+        int ki;                 // proportional gain
         int ref;                // reference value
-        int h;                  // Sensor Value
+        int v;                  // Sensor Value
         int y;                  // out PWM value
         int e;                  // Current error
     } StructPI;
     
-    // Variables
+    // Variablea initialization
     StructPI PI = {0, PWMTICKS, 0, KP, KI, REF, 0, 0, 0};
     float P = 0;    // Parte proporcional do controle
     float I = 0;    // Parte integrativa do controle
     
-    // @TODO REMOVER CODIGO DE TESTE
-    // CODIGO DE TESTE /////////////////////////////////////////////////////////
-//    int i = 0;
-//    osDelay(200);  
-//    while(1) {
-//        UARTprintf("Hello Controle! (%i)\n",i);
-//        i++;
-//        osDelay(500);   
-//    }
-    ////////////////////////////////////////////////////////////////////////////
-    
-    
-    
-    
-    
-    // @TODO Fazer logica de entrar velocidade em RPM e saida em porcentagem do
-    //       Duty Cycle
-    
-    
-    
-    
-    
-    
-    // Obter a velocidade atual
-    osMutexAcquire(mutexMeasurement_id, osWaitForever);
-    PI.h = measurement.velocidade;
-    osMutexRelease(mutexMeasurement_id);
+    while(1){
+        
+        // Obter a referencia atual
+        osMutexAcquire(mutexSetPointParams_id, osWaitForever);
+        PI.ref = configs.velocidade;
+        osMutexRelease(mutexSetPointParams_id);
+        
+        // Obter a velocidade atual
+        osMutexAcquire(mutexMeasurement_id, osWaitForever);
+        PI.v = measurement.velocidade;
+        osMutexRelease(mutexMeasurement_id);
 
-    // Regra de controle PI
-    PI.e = PI.ref - PI.h;   // Ganho atual
-    P = PI.e*PI.kp;         // Parte proporcional da regra
+        // Regra de controle PI
+        PI.e = PI.ref - PI.v;   // Ganho atual
+        P = PI.e*PI.kp;         // Parte proporcional da regra
+        
+        PI.integratState = PI.integratState + PI.e;
+        if (PI.integratState > PI.integratMax)
+        {
+            PI.integratState = PI.integratMax;
+        }
+        else if (PI.integratState < PI.integratMin)
+        {
+            PI.integratState = PI.integratMin;
+        }
+        I = PI.integratState*PI.ki; // Parte integrativa da regra
 
-    PI.integratState = PI.integratState + PI.e;
-    if (PI.integratState > PI.integratMax)
-    {
-        PI.integratState = PI.integratMax;
-    }
-    else if (PI.integratState < PI.integratMin)
-    {
-        PI.integratState = PI.integratMin;
-    }
-    I = PI.integratState*PI.ki; // Parte integrativa da regra
+        PI.y = (int)(P + I); // Regra de controle
 
-    PI.y = (int)(P + I); // Regra de controle
-
-    PI.y = PI.y + PWMOFFSET;   // Ajuste da saída
-    if (PI.y < PWMOFFSET)
-    {
-        PI.y = PWMOFFSET;
+        PI.y = PI.y + PWMOFFSET;   // Ajuste da saída
+        if (PI.y < PWMOFFSET)
+        {
+            PI.y = PWMOFFSET;
+        }
+        if (PI.y > PWMTICKS)
+        {
+            PI.y = PWMTICKS-1;
+        }
+        
+        PWMPulseWidthSet(PWM0_BASE, PWM_OUT_0, PI.y);
+        
+        if (DEBUG_MODE)
+            UARTprintf("Velocidade = %i  |  PWM = %i\n",PI.v,PI.y);   
     }
-    if (PI.y > PWMTICKS)
-    {
-        PI.y = PWMTICKS-1;
-    }
-    
-    PWMPulseWidthSet(PWM0_BASE, PWM_OUT_0, PI.y);
-    
-    if (DEBUG_MODE)
-        UARTprintf("Velocidade = %i  |  PWM = %i\n",PI.h,PI.y);
-    
 }
